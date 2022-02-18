aa806bebc7219b85cb72b95db59320634c63df1e
Ellaoui94 <abab010@student.kristiania.no>
2022-02-18 16:09:22 +0100

    client has RouterDom and Router and App has FrontPage og Question

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,34 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {BrowserRouter, Routes, Link, Route} from "react-router-dom";
 
-ReactDOM.render(<h1>fdfsf</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Kristiania Quiz</h1>
+      <Link to={"/question"}>Answer a question</Link>
+      <Link to={"/score"}>Show Score</Link>
+    </div>
+  );
+}
+
+function Question() {
+  return null;
+}
+
+function ShowScore() {
+  return null;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/question"} element={<Question />} />
+        <Route path={"/score"} element={<ShowScore />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+ReactDOM.render(<App />, document.getElementById("app"));
