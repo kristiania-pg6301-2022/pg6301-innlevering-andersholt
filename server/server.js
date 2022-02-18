c185d4eb626e4ad0542fa8acb9571be465bda7a8
andersholt <anho068@student.kristiania.no>
2022-02-18 16:17:20 +0100

    Express getRandomQ

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import { randomQuestion } from "./questions.js";
 
-const app = express()
+const app = express();
+
+app.get("/api/question", (req, res) => {
+  const { question, answers } = randomQuestion();
+
+  res.json({ question, answers });
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server started on port http://localhost:${server.address().port}`);
+  console.log(
+    `Server started on port http://localhost:${server.address().port}`
+  );
 });
