c4534f3076404178d9957a29fbe6a44afc056f87
andersholt <anho068@student.kristiania.no>
2022-02-18 16:30:11 +0100

    Express started on post question

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { randomQuestion } from "./questions.js";
+import { isCorrectAnswer, Questions, randomQuestion } from "./questions.js";
 
 const app = express();
 
@@ -7,6 +7,28 @@ app.get("/api/question", (req, res) => {
   const { id, category, question, answers } = randomQuestion();
   res.json({ id, question, answers, category });
 });
+
+app.post("/api/question", (req, res, next) => {
+  const { id, answer } = req.body;
+  const score = req.signedCookies.score
+    ? JSON.parse(req.signedCookies.score)
+    : { answered: 0, correct: 0 };
+  const question = Questions.find((q) => q.id == id);
+  if (!question) {
+    return res.status(404);
+  }
+
+  score.answered += 1;
+  if (isCorrectAnswer(question, answer)) {
+    score.correct += 1;
+    res.cookie("score"), JSON.stringify(score), { signed: true };
+    return res.json({ result: correct });
+  } else {
+    res.cookie("score", JSON.stringify(score), { signed: true });
+    return res.json({ result: "incorrect" });
+  }
+});
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(
     `Server started on port http://localhost:${server.address().port}`
