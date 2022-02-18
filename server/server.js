51d09ec00202c0f8c2803c77422dfa66265499a2
andersholt <anho068@student.kristiania.no>
2022-02-18 16:18:33 +0100

    Express getRandomQ

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ app.get("/api/question", (req, res) => {
 
   res.json({ question, answers });
 });
-
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(
     `Server started on port http://localhost:${server.address().port}`
