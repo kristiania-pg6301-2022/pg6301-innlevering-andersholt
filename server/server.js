039fc34c94e10135fe1759c1bd65642ecb1d3264
Ellaoui94 <abab010@student.kristiania.no>
2022-02-21 17:39:24 +0100

    app.post is sending true og false. still not tested from server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,26 @@ app.get("/api/question", (req, res) => {
   res.json({ id, question, answers, category });
 });
 
+// callback: (req, res) => {}
+app.post("/api/question", (req, res) => {
+  const { id, answer } = req.body;
+  const question = Questions.find((q) => q.id === id);
+
+  if (!question) {
+    return res.sendStatus(404);
+  }
+
+  if (isCorrectAnswer(question, answer)) {
+    return res.json({ result: "correct" });
+  } else {
+    return res.json({ result: "incorrect" });
+  }
+});
+
+//middleware for dist file
 app.use(express.static(path.resolve("../client/dist")));
+
+//middleware for correct path: /api or not
 app.use((req, res, next) => {
   if (req.method === "GET" && !req.path.startsWith("/api")) {
     return res.sendFile(path.resolve("../client/dist/index.html"));
@@ -18,6 +37,7 @@ app.use((req, res, next) => {
   }
 });
 
+//assigning port
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(
     `Server started on port http://localhost:${server.address().port}`
