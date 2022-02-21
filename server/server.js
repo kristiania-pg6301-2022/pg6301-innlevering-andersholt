3e166994f34763c8b539587f9dc4b0b70a65a0ba
Ellaoui94 <abab010@student.kristiania.no>
2022-02-21 17:12:48 +0100

    added middleware and random question is being printed

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ app.get("/api/question", (req, res) => {
   res.json({ id, question, answers, category });
 });
 
+app.use(express.static(path.resolve("../client/dist")));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(
     `Server started on port http://localhost:${server.address().port}`
