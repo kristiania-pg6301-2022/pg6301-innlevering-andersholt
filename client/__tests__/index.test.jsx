1d5573dff118c70fe7e4f6c70eac9446c268fc8b
andersholt <anho068@student.kristiania.no>
2022-02-23 18:58:35 +0000

    Refactor of index.jsx to index.jsx and application.jsx + Added simple jest test for FrontPage

---
+++ client/__tests__/index.test.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, MemoryRouter, Router, Routes } from "react-router-dom";
+
+import { FrontPage } from "../application.jsx";
+
+describe("index", () =>
+  it("displays homepage", () => {
+    const element = document.createElement("div");
+
+    ReactDOM.render(
+      <MemoryRouter>
+        <FrontPage />
+      </MemoryRouter>,
+      element
+    );
+
+    console.log(
+      <MemoryRouter>
+        <FrontPage></FrontPage>
+      </MemoryRouter>
+    );
+
+    expect(element.querySelector("h1").innerHTML).toEqual("Kristiania Quiz");
+    expect(element.innerHTML).toMatchSnapshot();
+  }));
---
+++ client/application.jsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export function FrontPage() {
+  return (
+    <div>
+      <h1>Kristiania Quiz</h1>
+      <div>
+        <Link to={"/question"}>Answer a question</Link>
+      </div>
+      <div>
+        <Link to={"/score"}>Show Score</Link>
+      </div>
+    </div>
+  );
+}
+
+export function Question() {
+  const [question, setQuestion] = useState();
+
+  useEffect(async () => {
+    const res = await fetch("/api/question");
+    setQuestion(await res.json());
+  }, []);
+
+  if (!question) {
+    return <div>Loading....</div>;
+  }
+
+  async function handleAnswer(answer) {
+    const { id } = question;
+    await fetch("api/question", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ id, answer }),
+    });
+  }
+
+  return (
+    <div>
+      <h1>{question.question}</h1>
+      {Object.keys(question.answers)
+        .filter((a) => question.answers[a])
+        .map((a) => (
+          <div key={a}>
+            <button onClick={() => handleAnswer(a)}>
+              {question.answers[a]}
+            </button>
+          </div>
+        ))}
+      <Link to={"/score"}>Show current score</Link>
+    </div>
+  );
+}
+
+/*function Q() {
+  const [question, setQuestion] = useState();
+
+  async function handle() {
+    const res = await fetch("/api/question");
+    setQuestion(await res.json());
+  }
+
+  if (!question) {
+    return <div>Loading....</div>;
+  }
+
+  return <Question question={question} />;
+}*/
+
+export function ShowScore() {
+  const [cookie, setCookie] = useState({});
+
+  useEffect(async () => {
+    const res = await fetch("/api/score");
+    setCookie(await res.json());
+  }, []);
+
+  const { answered, correct } = cookie;
+
+  return (
+    <div>
+      <h1>
+        You have: {correct} correct, out of: {answered} answers
+      </h1>
+      <div>
+        <Link to={"/question"}>Answer another question</Link>
+      </div>
+      <Link to={"/"}>Go Back to frontpage</Link>
+    </div>
+  );
+}
