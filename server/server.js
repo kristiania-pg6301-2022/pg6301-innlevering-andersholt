5adbdf4875ace89229c9dec6637295f893b9eab9
Ellaoui94 <abab010@student.kristiania.no>
2022-02-23 17:22:15 +0100

    Cookies are being saved after an answer. Cookieparser added

--- server/server.js
+++ server/server.js
@@ -2,32 +2,58 @@ import express from "express";
 import { isCorrectAnswer, Questions, randomQuestion } from "./questions.js";
 import * as path from "path";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-
 app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.get("/api/question", (req, res) => {
   const { id, category, question, answers } = randomQuestion();
   res.json({ id, question, answers, category });
 });
 
+/*
+app.use("api/question", CookieRouter);
+*/
+
 // callback: (req, res) => {}
 app.post("/api/question", (req, res) => {
   const { id, answer } = req.body;
+  const score = req.signedCookies.score
+    ? JSON.parse(req.signedCookies.score)
+    : {
+        answered: 0,
+        correct: 0,
+      };
   const question = Questions.find((q) => q.id === id);
 
   if (!question) {
     return res.sendStatus(404);
   }
 
+  score.answered += 1;
   if (isCorrectAnswer(question, answer)) {
-    console.log("right");
+    score.correct += 1;
+    res.cookie("score", JSON.stringify(score), { signed: true });
     return res.json({ result: "correct" });
   } else {
-    console.log("wrong");
+    res.cookie("score", JSON.stringify(score), { signed: true });
     return res.json({ result: "incorrect" });
   }
+});
+
+app.get("/api/score", (req, res) => {
+  const score = req.signedCookies.score
+    ? JSON.parse(req.signedCookies.score)
+    : {
+        answered: 0,
+        correct: 0,
+      };
+  res.json(score);
 });
 
 //middleware for dist file
