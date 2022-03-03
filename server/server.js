68006f0f1073091da54c521a82afc6cc665dc270
Ellaoui94 <abab010@student.kristiania.no>
2022-03-03 14:08:49 +0100

    Heroku test

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser("secret"));
 
 app.get("/api/question", (req, res) => {
   const { id, category, question, answers } = randomQuestion();
