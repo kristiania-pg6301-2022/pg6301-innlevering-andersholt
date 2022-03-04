0ffef4745faae552c3f2b2425c4b9791536ae27a
andersholt <anho068@student.kristiania.no>
2022-03-04 03:03:57 +0000

    Test for server done

--- client/__tests__/index.test.jsx
+++ client/__tests__/index.test.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import ReactDOM, { render } from "react-dom";
+import ReactDOM from "react-dom";
 import { MemoryRouter } from "react-router-dom";
-import express from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import request from "supertest";
 import "babel-polyfill";
 
-import { FrontPage, Question } from "../application.jsx";
-
-const app = express();
-app.use(bodyParser.json);
-app.use(cookieParser("test secret"));
-app.use("/question", Question);
+import { FrontPage } from "../application.jsx";
 
 describe("Quiz game Innlevering", () => {
   it("displays homepage", () => {
@@ -27,18 +18,4 @@ describe("Quiz game Innlevering", () => {
 
     expect(element.innerHTML).toMatchSnapshot();
   });
-
-  //  it("returns a random question", async () => {
-  //     const response = await request(app).get("/question/random").expect(200);
-  //     expect(response.body).toMatchObject({
-  //       id: expect.any(Number),
-  //       answers: expect.any(Object),
-  //       category: expect.any(String),
-  //     });
-  //     expect(response.body).not.toHaveProperty("correct_answers");
-  //   });
-  //
-  //   it("gives 404 on incorrect question", async () => {
-  //     await request(app).post("/quiz/answer").send({ id: -666 }).expect(404);
-  //   });
 });
