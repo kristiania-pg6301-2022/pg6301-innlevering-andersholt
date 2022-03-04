2bb04b843b69ea26eb5d2df47d592bbc86316bf1
andersholt <anho068@student.kristiania.no>
2022-03-04 01:52:29 +0000

    Servertest Works (kind of)

--- server/__tests__/index.test.jsx
+++ server/__tests__/index.test.jsx
@@ -1,27 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import request from "supertest";
-import { Question } from "../server.js";
 import cookieParser from "cookie-parser";
 
+export const Question = express.Router();
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use("/question", Question);
 
-beforeAll((done) => {
-  done();
-});
-
 afterAll((done) => {
-  // Closing the DB connection allows Jest to exit successfully.
   done();
 });
 
 describe("Post Endpoints", () => {
-  it("should create a new post", async (done) => {
+  it("should create a new post", async () => {
     const res = await request(app).get("/api/question");
     expect(res.statusCode).toEqual(404);
-    done();
   });
 });
--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ import * as path from "path";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-export const Question = express.Router();
 
 dotenv.config();
 
